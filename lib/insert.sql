9146054c413c94b2fb4f88c4ae2a923fe588334b
Zamzam <Zamzamhassan44@gmail.com>
2022-08-28 03:38:28 +0300

    organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 3, 'M', 'Green', 'sanguine', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'F', 'Black', 'sanguine', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 9, 'M', 'Grey', 'choleric', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant', 7,'M','Green','sanguine',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa',4,'M','White','melancholic',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch',8,'M','Brown','phlegmatic',True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy',2,'M','Red','melancholic',True);
+INSERT INTO bears (name) VALUES(NULL);
+
